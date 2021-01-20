69779337a05ffdd2b3d733640ded35aef57a8018
unknown <hyder1@gmail.com>
2021-01-20 17:13:05 -0600

    updates

--- .history/test/setup_20210108112929.js
+++
@@ -1,5 +0,0 @@
-let { expect } = require('chai')
-let supertest = require('supertest')
-
-global.expect = expect
-global.supertest = supertest
--- .history/test/setup_20210120161445.js
+++
@@ -1,8 +0,0 @@
-process.env.TZ = 'UTC'
-process.env.NODE_ENV = 'test'
-require('dotenv').config()
-let { expect } = require('chai')
-let supertest = require('supertest')
-
-global.expect = expect
-global.supertest = supertest
